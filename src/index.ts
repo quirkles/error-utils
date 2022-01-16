4edbd87fc5266892dbbb9604d67f061c8885dc46
quirkles <alexquirk@joinsherpa.com>
2022-01-16 17:39:55 -0500

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
 export { withErrorHandler } from './middleware';
+export {
+    ClientError,
+    ServerError,
+    UnauthorizedError,
+    CustomError,
+    UpstreamServiceError,
+    InternalServerError,
+    BadRequestError,
+    ForbiddenError,
+    NotFoundError,
+} from './errors';
